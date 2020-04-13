9814b8273cd664c8c96101f25eaadc8d3b6c2c7c
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2020-04-13 16:15:18 +0200

    Reorganisation

--- bb.py
+++ bb.py
@@ -1,93 +1,8 @@
 # Frank DaCosta: Writing BASIC adventure programs for the TRS-80
 # Basement and Beasties
 
-# Just for the record: list of valid directions
-# used as accessing "travel"
-# "default" is used as default for horizontal directions (not "up", "down", "magical")
-# if there is no "default", "False" returned
-horizontal_directions={"n", "ne", "e", "se", "s", "sw", "w", "nw"}
-directions= horizontal_directions.union({"default", "up", "down", "magical"})
-
-class Travel:
-  def __init__(self):
-    self.dict= {}
-  def get(self, dir):
-    if dir in self.dict:
-      return self.dict[key]
-    if dir in horizontal_directions and "default" in self.dict:
-      return self.dict["default"]
-    else:
-      return False
-  def put(self, key, value):
-    self.dict[key]= value
-  def merge(self, newvalues):
-    self.dict.update(newvalues)
-
-class Room:
-  def __init__(self, sDesc, lDesc):
-    self.sDesc= sDesc
-    self.lDesc= lDesc
-    self.travel= Travel();
-    self.Dark= False
-
-class DarkRoom(Room):
-  def __init__(self, sDesc, lDesc):
-    super().__init__(sDesc, lDesc)
-    self.Dark= True
-
-class Obstacle:
-  msg= {"stop": "You cannot go there"}
-  def __init__(self, sDesc):
-    self.sDesc= sDesc
-    self.visible= True
-    self.isOpen= False
-
-class InanimateObstacle(Obstacle):
-  msg= {}
-  msg.update (Obstacle.msg)
-  msg["nokey"]= "You have no key!"
-  msg["closing"]= "It slams shut and the lock catches."
-  def __init__(self, sDesc):
-    super().__init__(sDesc)
-
-class Door(InanimateObstacle):
-  msg= {}
-  msg.update (InanimateObstacle.msg)
-  msg["stop"]= "The door is closed and locked."
-  msg["opening"]= "The door swings open wide."
-  def __init__(self, sDesc="Door"):
-    super().__init__(sDesc)
-
-class Grate(InanimateObstacle):
-  msg= {}
-  msg.update (InanimateObstacle.msg)
-  msg["stop"]= "The grate is closed and locked."
-  msg["opening"]= "With a creak, the grate falls open."
-  def __init__(self, sDesc="Grate"):
-    super().__init__(sDesc)
-
-class Monster(Obstacle):
-  msg= {}
-  msg.update (Obstacle.msg)
-  msg["stop"]= "The monster blocks your way!"
-  def __init__(self, sDesc, lDesc):
-    super().__init__(sDesc)
-    self.lDesc= lDesc
-
-Grate28= Grate()
-Door411= Door()
-Door612= Door()
-
-Mantis= Monster("Mantis", "A Giant Mantis crouches nearby, ready to pounce!")
-Iguana= Monster("Iguana", "A huge Iguana paces restlessly nearby, keeping an eye on you!")
-Spider= Monster("Spider", "A giant white Spider mandibles twitching, towers above you!")
-Terror= Monster("Nameless Terror",
-  "The Nameless Terror arises from a pit" \
-  ", blocking your retreat with slimy tentacles!")
-
-class Death:
-  def __init__(self, sDesc):
-    self.sDesc= sDesc;
+from typing import Set, Dict
+from bb_types import *
 
 FallingDeath= Death("You fall to your Doom...");
 BurningDeath= Death("You burn in the flames!");
@@ -114,6 +29,44 @@ RushingStream = DarkRoom("Rushing Stream", "")
 SlimyCavern   = DarkRoom("Slimy Cavern", "")
 SteamyCave    = DarkRoom("Steamy Cave", "")
 FierySpire    = DarkRoom("Fiery Spire", "")
+
+allDarkRooms= \
+  {WeaponsRoom, LostBattle,  TombRoom,      OracleRoom,  TreasureVault, GuardPost, \
+   MazeA,       NarrowLedge, Cell,          Office,      LunchRoom,     CobwebRoom, \
+   MazeB,       MazeC,       RushingStream, SlimyCavern, SteamyCave,    FierySpire}
+
+allRooms= {BottomOfPit, Ruins}
+allRooms.update(allDarkRooms)
+
+Grate28= Grate()
+Door411= Door()
+Door612= Door()
+
+Torch= Obj(Ruins,      "Torch", "There is a burning torch here!")
+Crown= Obj(LostBattle, "Jewelled crown", "There is a crown of jewels here!")
+
+allObjs= {Torch, Crown}
+allTreasure= {Crown}
+
+allGates= [Grate28, Door411, Door612]
+
+Mantis= Monster("Mantis", "A Giant Mantis crouches nearby, ready to pounce!")
+Iguana= Monster("Iguana", "A huge Iguana paces restlessly nearby, keeping an eye on you!")
+Spider= Monster("Spider", "A giant white Spider mandibles twitching, towers above you!")
+Terror= Monster("Nameless Terror",
+  "The Nameless Terror arises from a pit" \
+  ", blocking your retreat with slimy tentacles!")
+
+allMonsters=  [Mantis, Iguana, Spider, Terror]
+
+LostBattle.monster= Mantis
+OracleRoom.monster= Terror
+CobwebRoom.monster= Spider
+SlimyCavern.monster= Iguana
+
+Ruins.gate= GuardPost.gate= Grate28
+LostBattle.gate= Cell.gate= Door411
+OracleRoom.gate= Office.gate= Door612
 
 # 148-150 [orig: 141-143]
 BottomOfPit.lDesc= \
@@ -256,10 +209,41 @@ FierySpire.travel.put("up", SteamyCave)
 class PlayerState:
   def __init__(self):
     self.reset()
+
   def reset(self):
-    self.visitedRooms= {}
-    self.ownedItems= {}
+    self.visitedRooms= set()
+    self.ownedObjs= set()
     self.position= BottomOfPit
+  # reset state of the inanimated obstacles
+    self.resetGates()
+    self.resetMonsters()
+    self.resetPositions()
+
+  def resetMonsters(self):
+    self.monsterState= {}
+    for m in allMonsters:
+       self.monsterState[m]= isAlive
+
+  def resetGates(self):
+    self.gateState= {}
+    for g in allGates:
+       self.gateState[g]= isClosed
+
+  # the player and the rooms can have belongings,
+  # the monsters cannot (for now)
+  # now everyone loses everything
+  # then every object goes to its original place
+  def resetPositions(self):
+    self.belongings: Dict[object, Set] = dict()
+    self.belongings[Player]= set()
+    for r in allRooms:
+       self.belongings[r]= set()
+    self.positions= dict()
+    for o in allObjs:
+      toRoom= o.originalPosition
+      self.positions[o]= toRoom
+      self.belongings[toRoom].add(o)
+
   def describeRoom(self, room="Default", which="Default"):
     if room=="Default": room= self.position
     if which=="Default":
@@ -268,10 +252,43 @@ class PlayerState:
     if which=="Short":  print (room.sDesc)
     elif which=="Long": print (room.lDesc)
     else:               print ("Serious problem in method PlayerState.describeRoom")
+    self.listObjsInRoom(room)
+
+  def listObjsInRoom(self, room="Default", separator=True):
+    if room=="Default": room= self.positions[Player];
+    if len(self.belongings[room])==0: return
+    if (separator): print()
+    for o in self.belongings[room]:
+      print (o.lDesc)
+
+  def enterRoom(self, intoroom):
+    self.position= intoroom
+    if intoroom.isDark:
+      print ("It is pitch dark! You may fall into a pit!")
+      # random death
+    else:
+      self.describeRoom(intoroom)
+      self.visitedRooms.add(intoroom)
+#endclass PlayerState
 
 ps= PlayerState()
 
-ps.describeRoom(Office);
-ps.describeRoom(which='Long');
-ps.describeRoom(Ruins,"Short");
-ps.describeRoom(SlimyCavern,"Long");
+"""
+ps.describeRoom(Office)
+ps.describeRoom(which="Long")
+ps.describeRoom(Ruins,"Short")
+ps.describeRoom(SlimyCavern,"Long")
+print(ps.monsterState)
+"""
+
+print ("=== Test: enterRoom(Ruins) ===")
+ps.enterRoom(Ruins)
+
+print ("\n=== Test: enterRoom(BottomOfPit) ===")
+ps.enterRoom(BottomOfPit)
+
+print ("\n=== Test: enterRoom(Ruins) ===")
+ps.enterRoom(Ruins)
+
+print ("\n=== Test: enterRoom(Office) ===")
+ps.enterRoom(Office)
---
+++ bb_types.py
@@ -0,0 +1,131 @@
+# Frank DaCosta: Writing BASIC adventure programs for the TRS-80
+# Basement and Beasties
+
+# Type and constant-definitions that aren't specific to this game
+# Also declaritions of variablas "allRooms", "allDarkRooms", "allGates",
+# "allMonsters", "allObjs", "allTreasures"
+
+from typing import Set
+
+# directions: keys to "travel" dictionaries
+# "default" is used as default for horizontal directions (not "up", "down", "magical")
+# if there is no "default", "False" returned
+
+horizontal_directions={"n", "ne", "e", "se", "s", "sw", "w", "nw"}
+directions= horizontal_directions.union({"default", "up", "down", "magical"})
+
+class Travel:
+  def __init__(self):
+    self.dict= {}
+  def get(self, dir):
+    if dir in self.dict:
+      return self.dict[key]
+    if dir in horizontal_directions and "default" in self.dict:
+      return self.dict["default"]
+    else:
+      return False
+  def put(self, key, value):
+    self.dict[key]= value
+  def merge(self, newvalues):
+    self.dict.update(newvalues)
+
+class Room:
+  def __init__(self, sDesc, lDesc):
+    self.sDesc= sDesc
+    self.lDesc= lDesc
+    self.travel= Travel();
+    self.isDark= False
+    self.monster= False
+    self.gate= False
+  def __repr__(self):
+    return self.sDesc
+
+allRooms: Set[Room]
+
+class DarkRoom(Room):
+  def __init__(self, sDesc, lDesc):
+    super().__init__(sDesc, lDesc)
+    self.isDark= True
+
+allDarkRooms: Set[DarkRoom]
+
+class Obstacle:
+  msg= {"stop": "You cannot go there"}
+  def __init__(self, sDesc):
+    self.sDesc= sDesc
+    self.visible= True
+    self.isOpen= False
+  def __repr__(self):
+    return self.sDesc
+
+# these are just names to track states of the obstacles
+isOpen=   True
+isClosed= False
+isDead=   True
+isAlive=  False
+
+# Base class for Door and Grate
+class Gate(Obstacle):
+  msg= {}
+  msg.update (Obstacle.msg)
+  msg["nokey"]= "You have no key!"
+  msg["closing"]= "It slams shut and the lock catches."
+  def __init__(self, sDesc):
+    super().__init__(sDesc)
+
+allGates: Set[Gate]
+
+class Door(Gate):
+  msg= {}
+  msg.update (Gate.msg)
+  msg["stop"]= "The door is closed and locked."
+  msg["opening"]= "The door swings open wide."
+  def __init__(self, sDesc="Door"):
+    super().__init__(sDesc)
+
+class Grate(Gate):
+  msg= {}
+  msg.update (Gate.msg)
+  msg["stop"]= "The grate is closed and locked."
+  msg["opening"]= "With a creak, the grate falls open."
+  def __init__(self, sDesc="Grate"):
+    super().__init__(sDesc)
+
+class Monster(Obstacle):
+  msg= {}
+  msg.update (Obstacle.msg)
+  msg["stop"]= "The monster blocks your way!"
+  def __init__(self, sDesc, lDesc):
+    super().__init__(sDesc)
+    self.lDesc= lDesc
+
+allMonsters: Set[Monster]
+
+# Objects (objs): the things you can take/drop/use
+# The constant data only
+# the actual position of the objects is variable so
+# it will be stored elsewhere
+class Obj:
+  def __init__(self, originalPosition, sDesc, lDesc):   
+    self.originalPosition= originalPosition
+    self.sDesc= sDesc
+    self.lDesc= lDesc
+    self.isTreasure= False
+  def __repr__(self):
+    return self.sDesc
+
+allObjs: Set[Obj]
+
+class Treasure(Obj):
+  def __init__(self, originalPosition, sDesc, lDesc):
+    super().__init__(originalPosition, sDesc, lDesc)
+    self.isTreasure= True
+
+allTreasures: Set[Obj]
+
+# special name to represent the locations of objects hold by the playe
+Player= object()
+
+class Death:
+  def __init__(self, sDesc):
+    self.sDesc= sDesc;
