806f9ea75ab0d69bc702ba09ea1003ff1fd415a3
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2024-06-28 16:03:41 +0200

    Original upload

---
+++ get_io_layers.pl
@@ -0,0 +1,32 @@
+#!/usr/local/bin/perl
+
+# usage: require "get_io_layers.pL"
+# printf("IO Layers of somefile: %s\n", GetIoLayers($fh));
+
+# result:
+# Layers of somefile:
+#   Input:  unix-201200 perlio-c81200
+#   Output: unix-201200 perlio-c81200 encoding(utf-8-strict)-c89200
+
+sub GetIoLayers {
+    my ($fh)= @_;
+
+    my @lst= PerlIO::get_layers($fh, details=>1);
+    my $len= scalar(@lst);
+    my $s= "\n    Input:  ";
+    for (my $i= 0; $i<$len; $i += 3) {
+        if ($lst[$i+1]) { $s .= sprintf ("%s(%s)-%x ", $lst[$i], $lst[$i+1], $lst[$i+2]); }
+        else            { $s .= sprintf ("%s-%x ", $lst[$i], $lst[$i+2]); }
+    }
+
+    @lst= PerlIO::get_layers($fh, details=>1,output=>1);
+    $len= scalar(@lst);
+    $s .= "\n    Output: ";
+    for (my $i= 0; $i<$len; $i += 3) {
+        if ($lst[$i+1]) { $s .= sprintf ("%s(%s)-%x ", $lst[$i], $lst[$i+1], $lst[$i+2]); }
+        else            { $s .= sprintf ("%s-%x ", $lst[$i], $lst[$i+2]); }
+    }
+    return $s;
+}
+
+return true;
