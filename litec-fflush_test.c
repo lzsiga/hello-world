6e74e610852af4b7df9adee6ce669a620bf3ad63
Lorinczy Zsigmond <lzsiga@t-online.hu>
2024-07-24 06:00:58 +0200

    With stdio functions it works

--- litec-fflush_test.c
+++ litec-fflush_test.c
@@ -9,19 +9,19 @@
 #include <d3d9.h>
 
 function main() {
-    var fhandle_n = 0;
+    FILE *fhandle_n = NULL;
 
     level_load(NULL);
     wait(2);
 
-    fhandle_n = file_open_write ("bubu.txt");
+    fhandle_n = fopen("bubu.txt", "at");
 diag_var("\nfhandle_n: %6.3f",fhandle_n);
 
-    file_str_write(fhandle_n, "bubu\n\r");
+    fprintf(fhandle_n, "bubu\n");
 diag("\njon");
 diag_var("\nfhandle_n: %6.3f",fhandle_n);
     fflush(fhandle_n);        // itt akad el...
 diag("\nmegy");
 
-    file_close(fhandle_n);
+    fclose(fhandle_n);
 }
