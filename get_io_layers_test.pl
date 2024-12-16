dd94c270adb616ce77ad4164f2b2456c110f9c1a
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2024-12-16 17:31:39 +0100

    Tests for 'raw' and 'pop'

--- get_io_layers_test.pl
+++ get_io_layers_test.pl
@@ -16,4 +16,12 @@ printf("After another binmode: %s\n", GetIoLayers(*STDERR));
 #After another binmode:
 #   Input:  unix-201200 perlio-c91200 encoding(utf-8-strict)-c89200 encoding(iso-8859-1)-c89200
 #   Output: unix-201200 perlio-c91200 encoding(utf-8-strict)-c89200 encoding(iso-8859-1)-c89200
-#I don't know how to _remove_ a layer :(
+
+binmode(STDERR, ':pop');
+printf("After ':pop': %s\n", GetIoLayers(*STDERR));
+
+binmode(STDERR, ':raw');
+printf("After ':raw': %s\n", GetIoLayers(*STDERR));
+
+printf(STDERR "testwrite\n");
+printf("After write: %s\n", GetIoLayers(*STDERR));
