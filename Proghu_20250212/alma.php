d3fba02abb54c0d440d5d21025c2542f99c1225b
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2025-02-13 12:51:42 +0100

    Meg debug

--- Proghu_20250212/alma.php
+++ Proghu_20250212/alma.php
@@ -1,4 +1,9 @@
 <?php
+  ini_set('display_errors', '1');
+  error_reporting(E_ALL);
+
+  header('Content-Type: text/html; charset=UTF-8');
+
   printf ('<P>%s:%s var_dump($_POST):</p>', basename(__FILE__), __LINE__);
   print ("<pre>");
   var_dump ($_POST);
@@ -11,21 +16,20 @@
   $username = strrev($username); // megforditja a kiírás sorrendjét
   $password = strrev($password); // megforditja a kiírás sorrendjét
 
-
   echo $username;
   echo $password;
-
 
 #------------Ez a megoldás hibás----------------------------------------
 
 
-# Ha valamilyen értéket rendelek a egyes vázozókhoz, (a fentiektől eltérően)   mind a kettőt kiírja  az index oldalon egymás után? Egy ellenőrzés lefuttatása után szeretném az eredményeket közölni. Ez csak egy lebutított próbálkozás. pld. az alábbi megoldás hibás!
-/*
+# Ha valamilyen értéket rendelek a egyes változókhoz, (a fentiektől eltérően)
+# mind a kettőt kiírja  az index oldalon egymás után?
+# Egy ellenőrzés lefuttatása után szeretném az eredményeket közölni.
+# Ez csak egy lebutított próbálkozás. pld. az alábbi megoldás hibás!
+
   $username = "Hibás név!";
   $password = "Hibás jelszó";
 
-
   echo $username;
   echo $password;
-*/
 ?>
