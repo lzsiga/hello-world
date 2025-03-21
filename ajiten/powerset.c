bb8b85e27ea9182a47b11ece201c1131e1ce824f
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2025-03-21 18:54:23 +0100

    state #90

--- ajiten/powerset.c
+++ ajiten/powerset.c
@@ -1,61 +1,37 @@
-/* powerset.c */
-
-#include <limits.h>
 #include <stdio.h>
-
-#ifndef LONG_BIT
-#define LONG_BIT (CHAR_BIT * sizeof(long))
-#endif
-
-/* sample usage: ./powerset apple pear cherry
-   output (2^3==8 lines):
-     []
-     [apple]
-     ...
-     [apple,pear,cherry]
-
-   sample recursive usage: ./powerset $(./powerset apple pear cherry)
-   output (2^8==256 lines):
-     []
-     [[]]
-     ...
-     [[],[apple],[pear],[apple,pear],[cherry],[apple,cherry],[pear,cherry],[apple,pear,cherry]]
- */
-
-static void PrintSubSet (unsigned long cnt, int nelems, const char **pelems) {
-    int i;
-    unsigned long mask= 1;
-    int nprinted= 0;
-    FILE *f= stdout;
-
-    fputc ('[', f);
-    for (i=0; i<=nelems; ++i, mask<<=1) {
-        if ((cnt&mask)!=0) {
-            if (nprinted>0) fputc(',', f);
-            printf ("%s", pelems[i]);
-            ++nprinted;
+int count=0;
+struct node {
+    char *s;
+    struct node* prev;
+};
+char *arrv[] = {"a", "b", "c", '\0'};
+int arrc = sizeof(arrv)/sizeof(arrv[0]) - 1;
+int call_no =0;
+void powerset(char **v, int n, struct node *up, int call_no, int val)
+{
+    struct node me = {NULL, NULL};     
+    printf("\n---(1)char **v:%s, int n:%d, node *up:%p, call_no:%d, val:%d", *v,n,up,call_no,val);                                      
+    if (!n) {
+        printf("  count> %d", ++count);
+        putchar('[');
+        while (up) {
+            printf("%s", up->s);
+            up = up->prev;
+            printf(" (2)node *up: %p", up); 
         }
+        puts("]");
+    } else {
+        me.s = *v;
+        me.prev = up; 
+        printf(" (3)me:%p", (void*)&me);   
+        powerset(v + 1, n - 1, up, ++call_no, 1);
+        powerset(v + 1, n - 1, &me, ++call_no, 0);
     }
-    fputc (']', f);
-    fputc ('\n', f);
 }
-
-int main (int argc, char **argv) {
-    int nmax= LONG_BIT;
-    int nelems= argc-1;
-    const char **pelems= (const char **)argv+1;
-    unsigned long cnt, cntmax;
-
-    if (nelems>nmax) {
-        fprintf (stderr, "Too many elements (%d), truncating to %d\n",
-            nelems, nmax);
-        nelems= nmax;
-    }
-
-    if (nelems==0) cntmax= 0;
-    else           cntmax= ((unsigned long)-1L)>>(LONG_BIT-nelems);
-    for (cnt= 0; cnt<=cntmax; ++cnt) {
-        PrintSubSet (cnt, nelems, pelems);
-    }
+    
+int main(int argc, char **argv)
+{
+    printf("size of char: %ld, size of char*: %ld, size of 'struct node': %ld, size of 'struct node pointer':%ld", sizeof(char), sizeof(char *), sizeof (struct node), sizeof(struct node *()));
+    powerset(arrv, arrc, 0, ++call_no,1);
     return 0;
 }
