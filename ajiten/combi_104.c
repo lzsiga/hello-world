422833301d90f1275d1d18d4455e6bd20062704e
Lorinczy Zsigmond <lzsiga@t-online.hu>
2025-05-02 13:03:54 +0200

    Pass subset_number's address

--- ajiten/combi_104.c
+++ ajiten/combi_104.c
@@ -19,21 +19,22 @@ static void printCombination(int subset[], int r, int fn_type, int subset_number
 // arr/arrv  set of elements                              //n  number of set elements                     //r  number of subset elements
 // index     process set elements, starting from 'index'  //k  already generated subset elements (k<=r)
 // subset    buffer for subset elements
-void generateCombinationsRecursive(int arr[], int n, int r, int index, int k, int *subset, int subset_number) {
+void generateCombinationsRecursive(int arr[], int n, int r, int index, int k, int *subset, int *subset_number) {
     if (k == r) {
-        printCombination(subset, r, 0, subset_number);
+        ++*subset_number;
+        printCombination(subset, r, 0, *subset_number);
         return;
     }
     for (int i = index; i < n; i++) {
         subset[k] = arr[i];
-        generateCombinationsRecursive(arr, n, r, i + 1, k + 1, subset, ++subset_number);
+        generateCombinationsRecursive(arr, n, r, i + 1, k + 1, subset, subset_number);
     }
 }
 
 // Wrapper Function for Recursion
 void runRecursive(int arr[], int n, int r) {
     int *subset = (int *)malloc(r * sizeof(int)); int subset_number = 0;
-    generateCombinationsRecursive(arr, n, r, 0, 0, subset, 0);
+    generateCombinationsRecursive(arr, n, r, 0, 0, subset, &subset_number);
     free(subset);
 }
 // Brute Force (Loop-Based) Approach
@@ -59,7 +60,7 @@ int runLoopBased(int arr[], int n, int r) {
 int main() {
     int arr[] = {1, 2, 3, 4, 12, 9, 5, 70}; // Example array
     int arrc = sizeof(arr) / sizeof(arr[0]);
-    int r = (int)ceil((double)(arrc)/3);
+    int r = (arrc+2)/3;
 
     clock_t start, end;
     double time_rec, time_loop;
