1db5f45eaecd305f4205c080610ffc5f27fb2030
Lorinczy Zsigmond <lzsiga@t-online.hu>
2025-05-17 17:24:39 +0200

    Elementary fixes

--- ajiten/combi_151.c
+++ ajiten/combi_151.c
@@ -5,62 +5,61 @@
 #include <math.h>
 #include <assert.h>
 
-typedef struct StatData {    
-    int subset_number;   
+typedef struct StatData {
+    int subset_number;
 } StatData;
 
 // Function to print a combination
 static void printCombination(int subset[], int r, int fn_type, int subset_number) {
-     if (!fn_type) 
-         printf("0_rec:>%d:{",subset_number); 
-     else 
+     if (!fn_type)
+         printf("0_rec:>%d:{", subset_number);
+     else
          printf("1_loop>%d:{", subset_number);
-     for (int i = 0; i < r; i++)
-         if(i<r-1)               
-             printf("%d,", subset[i]);        
-         else               
+
+     for (int i = 0; i < r; i++) {
+         if(i<r-1)
+             printf("%d,", subset[i]);
+         else
              printf("%d", subset[i]);
-         printf("}\n");
+     }
+     printf("}\n");
 }
 
 // Recursive function to generate combinations
-// arr/arrv  set of elements                              
-// n         number of set elements                     
+// arr/arrv  set of elements
+// n         number of set elements
 // r         number of subset elements
-// index     process set elements, starting from 'index'  
+// index     process set elements, starting from 'index'
 // k         already generated subset elements (k<=r)
 // subset    buffer for subset elements
-int generateCombinationsRecursive(int arr[], int n, int r, int index, int k, int *subset, StatData *sdp) {
-    clock_t start, end;    
-    double time_rec, time_loop;
-    if (k == r) {   
-        ++sdp->subset_number;        
-        //printCombination(subset, r, 0, sdp->subset_number);                      //line #38 
+void generateCombinationsRecursive(int arr[], int n, int r, int index, int k, int *subset, StatData *sdp) {
+    if (k == r) {
+        ++sdp->subset_number;
+        printCombination(subset, r, 0, sdp->subset_number);                      //line #38
         return;
     }
-    for (int i = index; i < n; i++) {        
+    int lim= n-(r-k-1);
+    for (int i = index; i < lim; i++) {
         subset[k] = arr[i];
         //start = clock();
         // Measure Time, for all subsets, starting with element at a given index position
-        generateCombinationsRecursive(arr, n, r, i + 1, k + 1, subset, sdp);    
+        generateCombinationsRecursive(arr, n, r, i + 1, k + 1, subset, sdp);
         //end = clock();
         //time_rec = ((double)(end - start)) / CLOCKS_PER_SEC;
         //printf("\ntime_rec:%f, for start index: %d", time_rec, i);
     }
-    return sdp->subset_number;
 }
 
 // Wrapper Function for Recursion
-int runRecursive(int arr[], int n, int r, StatData *sdp) {  
-    int subset[r];       
-    int count_subset_rec;    
-    count_subset_rec = generateCombinationsRecursive(arr, n, r, 0, 0, subset, sdp); 
-    return count_subset_rec;
+int runRecursive(int arr[], int n, int r, StatData *sdp) {
+    int subset[r];
+    generateCombinationsRecursive(arr, n, r, 0, 0, subset, sdp);
+    return sdp->subset_number;
 }
 // Brute Force (Loop-Based) Approach
 int runLoopBased(int arr[], int n, int r) {
-    //assert(r==3);
-    int *subset = (int *)malloc(r * sizeof(int));
+    assert(r==7);
+    int subset[7];
     int subset_number=0;
     for (int i = 0; i < n - 6; i++) {
         subset[0] = arr[i];
@@ -76,8 +75,8 @@ int runLoopBased(int arr[], int n, int r) {
                             subset[5] = arr[o];
                             for (int p = o + 1; p < n; p++) {
                                 subset[6] = arr[p];
-                                //printCombination(subset, r, 1, ++subset_number); //commented to allow correct comparison, of run-time   //line #79
                                 ++subset_number;
+                                printCombination(subset, r, 1, subset_number); //commented to allow correct comparison, of run-time   //line #79
                             }
                         }
                     }
@@ -85,27 +84,27 @@ int runLoopBased(int arr[], int n, int r) {
             }
         }
     }
-    free(subset);
     return subset_number;
 }
+
 // Main Function for Execution
 int main() {
-   int arr[] = {1, 2, 3, 4, 12, 9, 5, 70, 13, 19, 27, 7, 23, 26, 24, 11, 111, 67};     
-   int arrc = sizeof(arr) / sizeof(arr[0]);    
-   int r = 7;// (arrc+2)/3;   
-  
+   int arr[] = {1, 2, 3, 4, 12, 9, 5, 70, 13, 19, 27, 7, 23, 26, 24, 11, 111, 67};
+   int arrc = sizeof(arr) / sizeof(arr[0]);
+   int r = 7;// (arrc+2)/3;
+
    int count_rec, count_loop;
    StatData sd;
 
-   clock_t start, end;    
+   clock_t start, end;
    double time_rec, time_loop;
 
    memset(&sd, 0, sizeof sd);
 
    // Measure Time for Recursive Approach
-   start = clock();    
-   count_rec = runRecursive(arr, arrc, r, &sd);    
-   end = clock();    
+   start = clock();
+   count_rec = runRecursive(arr, arrc, r, &sd);
+   end = clock();
    time_rec = ((double)(end - start)) / CLOCKS_PER_SEC;
 
    // Measure Time for Loop-Based Approach
@@ -115,13 +114,13 @@ int main() {
    time_loop = ((double)(end - start)) / CLOCKS_PER_SEC;
 
    // Print Results
-   printf("Number of Combinations (Recursive): %d\n", count_rec);
-   printf("Execution Time (Recursive): %f seconds\n", time_rec);
+   fprintf(stderr, "Number of Combinations (Recursive): %d\n", count_rec);
+   fprintf(stderr, "Execution Time (Recursive): %f seconds\n", time_rec);
 
-   printf("Number of Combinations (Loop-Based): %d\n", count_loop);
-   printf("Execution Time (Loop-Based): %f seconds\n", time_loop);
+   fprintf(stderr, "Number of Combinations (Loop-Based): %d\n", count_loop);
+   fprintf(stderr, "Execution Time (Loop-Based): %f seconds\n", time_loop);
 
-   printf("Ratio of the time taken by the Recursive version, to the time taken by the Loop based approach: %f times\n", time_rec/time_loop);
+   fprintf(stderr, "Ratio of the time taken by the Recursive version, to the time taken by the Loop based approach: %f times\n", time_rec/time_loop);
 
    return 0;
 }
